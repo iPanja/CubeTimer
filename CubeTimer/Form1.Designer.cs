6e113260b2b32745fb76ed9d43f047b4844e9524
iPanja <fhenneman@comcast.net>
2019-06-22 17:56:07 -0400

    Add files via upload

---
+++ CubeTimer/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace CubeTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.scrambleLabel = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroPanel2 = new MetroFramework.Controls.MetroPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.inspectionDropdown = new MetroFramework.Controls.MetroComboBox();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.metroPanel4 = new MetroFramework.Controls.MetroPanel();
+            this.removeFromHistory = new MetroFramework.Controls.MetroButton();
+            this.historyList = new System.Windows.Forms.ListBox();
+            this.metroPanel3 = new MetroFramework.Controls.MetroPanel();
+            this.averageLabel = new MetroFramework.Controls.MetroLabel();
+            this.bestLabel = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroPanel1.SuspendLayout();
+            this.metroPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.metroPanel4.SuspendLayout();
+            this.metroPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.metroPanel1.Controls.Add(this.scrambleLabel);
+            this.metroPanel1.Controls.Add(this.metroLabel1);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(23, 73);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(1088, 38);
+            this.metroPanel1.TabIndex = 0;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // scrambleLabel
+            // 
+            this.scrambleLabel.Location = new System.Drawing.Point(3, -1);
+            this.scrambleLabel.Name = "scrambleLabel";
+            this.scrambleLabel.Size = new System.Drawing.Size(1080, 37);
+            this.scrambleLabel.TabIndex = 3;
+            this.scrambleLabel.Text = "Scramble: loading...";
+            this.scrambleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(4, 0);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(0, 0);
+            this.metroLabel1.TabIndex = 2;
+            // 
+            // metroPanel2
+            // 
+            this.metroPanel2.Controls.Add(this.panel1);
+            this.metroPanel2.Controls.Add(this.metroLabel4);
+            this.metroPanel2.Controls.Add(this.label1);
+            this.metroPanel2.Controls.Add(this.metroPanel4);
+            this.metroPanel2.Controls.Add(this.metroPanel3);
+            this.metroPanel2.Controls.Add(this.metroLabel3);
+            this.metroPanel2.HorizontalScrollbarBarColor = true;
+            this.metroPanel2.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel2.HorizontalScrollbarSize = 10;
+            this.metroPanel2.Location = new System.Drawing.Point(23, 117);
+            this.metroPanel2.Name = "metroPanel2";
+            this.metroPanel2.Size = new System.Drawing.Size(1088, 491);
+            this.metroPanel2.TabIndex = 1;
+            this.metroPanel2.VerticalScrollbarBarColor = true;
+            this.metroPanel2.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel2.VerticalScrollbarSize = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.inspectionDropdown);
+            this.panel1.Location = new System.Drawing.Point(223, 453);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(643, 38);
+            this.panel1.TabIndex = 6;
+            // 
+            // inspectionDropdown
+            // 
+            this.inspectionDropdown.FormattingEnabled = true;
+            this.inspectionDropdown.ItemHeight = 23;
+            this.inspectionDropdown.Location = new System.Drawing.Point(3, 3);
+            this.inspectionDropdown.Name = "inspectionDropdown";
+            this.inspectionDropdown.Size = new System.Drawing.Size(121, 29);
+            this.inspectionDropdown.TabIndex = 0;
+            this.inspectionDropdown.UseSelectable = true;
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel4.Location = new System.Drawing.Point(878, 1);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(206, 37);
+            this.metroLabel4.TabIndex = 4;
+            this.metroLabel4.Text = "History";
+            this.metroLabel4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(222, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(644, 460);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // metroPanel4
+            // 
+            this.metroPanel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.metroPanel4.Controls.Add(this.removeFromHistory);
+            this.metroPanel4.Controls.Add(this.historyList);
+            this.metroPanel4.HorizontalScrollbarBarColor = true;
+            this.metroPanel4.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel4.HorizontalScrollbarSize = 10;
+            this.metroPanel4.Location = new System.Drawing.Point(872, 0);
+            this.metroPanel4.Name = "metroPanel4";
+            this.metroPanel4.Size = new System.Drawing.Size(216, 491);
+            this.metroPanel4.TabIndex = 4;
+            this.metroPanel4.VerticalScrollbarBarColor = true;
+            this.metroPanel4.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel4.VerticalScrollbarSize = 10;
+            // 
+            // removeFromHistory
+            // 
+            this.removeFromHistory.Location = new System.Drawing.Point(5, 463);
+            this.removeFromHistory.Name = "removeFromHistory";
+            this.removeFromHistory.Size = new System.Drawing.Size(206, 23);
+            this.removeFromHistory.TabIndex = 8;
+            this.removeFromHistory.Text = "Remove";
+            this.removeFromHistory.UseSelectable = true;
+            this.removeFromHistory.Click += new System.EventHandler(this.removeFromHistory_Click);
+            // 
+            // historyList
+            // 
+            this.historyList.FormattingEnabled = true;
+            this.historyList.Location = new System.Drawing.Point(5, 40);
+            this.historyList.Name = "historyList";
+            this.historyList.Size = new System.Drawing.Size(206, 420);
+            this.historyList.TabIndex = 7;
+            // 
+            // metroPanel3
+            // 
+            this.metroPanel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.metroPanel3.Controls.Add(this.averageLabel);
+            this.metroPanel3.Controls.Add(this.bestLabel);
+            this.metroPanel3.Controls.Add(this.metroLabel5);
+            this.metroPanel3.HorizontalScrollbarBarColor = true;
+            this.metroPanel3.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel3.HorizontalScrollbarSize = 10;
+            this.metroPanel3.Location = new System.Drawing.Point(0, 0);
+            this.metroPanel3.Name = "metroPanel3";
+            this.metroPanel3.Size = new System.Drawing.Size(216, 491);
+            this.metroPanel3.TabIndex = 3;
+            this.metroPanel3.VerticalScrollbarBarColor = true;
+            this.metroPanel3.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel3.VerticalScrollbarSize = 10;
+            // 
+            // averageLabel
+            // 
+            this.averageLabel.AutoSize = true;
+            this.averageLabel.Location = new System.Drawing.Point(4, 79);
+            this.averageLabel.Name = "averageLabel";
+            this.averageLabel.Size = new System.Drawing.Size(84, 19);
+            this.averageLabel.TabIndex = 8;
+            this.averageLabel.Text = "Average: n/a";
+            // 
+            // bestLabel
+            // 
+            this.bestLabel.AutoSize = true;
+            this.bestLabel.Location = new System.Drawing.Point(4, 41);
+            this.bestLabel.Name = "bestLabel";
+            this.bestLabel.Size = new System.Drawing.Size(59, 19);
+            this.bestLabel.TabIndex = 7;
+            this.bestLabel.Text = "Best: n/a";
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel5.Location = new System.Drawing.Point(3, 0);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(208, 37);
+            this.metroLabel5.TabIndex = 6;
+            this.metroLabel5.Text = "Statistics";
+            this.metroLabel5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel3.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel3.Location = new System.Drawing.Point(213, 0);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(659, 491);
+            this.metroLabel3.TabIndex = 2;
+            this.metroLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroLabel3.UseCustomForeColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 631);
+            this.Controls.Add(this.metroPanel2);
+            this.Controls.Add(this.metroPanel1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Cube Timer";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.metroPanel1.ResumeLayout(false);
+            this.metroPanel1.PerformLayout();
+            this.metroPanel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.metroPanel4.ResumeLayout(false);
+            this.metroPanel3.ResumeLayout(false);
+            this.metroPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroLabel scrambleLabel;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroPanel metroPanel2;
+        private MetroFramework.Controls.MetroPanel metroPanel4;
+        private MetroFramework.Controls.MetroPanel metroPanel3;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private System.Windows.Forms.Label label1;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private System.Windows.Forms.ListBox historyList;
+        private MetroFramework.Controls.MetroButton removeFromHistory;
+        private MetroFramework.Controls.MetroLabel averageLabel;
+        private MetroFramework.Controls.MetroLabel bestLabel;
+        private System.Windows.Forms.Panel panel1;
+        private MetroFramework.Controls.MetroComboBox inspectionDropdown;
+    }
+}
+
---
+++ CubeTimer/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CubeTimer{
+    public partial class Form1 : MetroFramework.Forms.MetroForm{
+        bool enabled = false;
+        Timer timer;
+        Timer iTimer;
+        Stopwatch watch;
+        BindingList<double> history;
+        List<String> turns = new List<String>{ "U", "D", "L", "R", "B", "F" };
+        List<String> modifiers = new List<string> { "", "\'", "2" };
+        List<int> inspections = new List<int> { 0, 3, 5, 10, 15 };
+        int inspectionTime;
+
+        public Form1(){
+            InitializeComponent();
+            watch = new Stopwatch();
+            timer = new Timer();
+            timer.Interval = 10;
+            timer.Tick += new EventHandler(updateTime);
+            //
+            iTimer = new Timer();
+            iTimer.Interval = 10;
+            iTimer.Tick += new EventHandler(updateInspection);
+            //
+            history = new BindingList<double>();
+            historyList.DataSource = history;
+            scrambleLabel.Text = "Scramble: " + generateScramble();
+            Console.Out.WriteLine("L2".First() != "L2".First());
+            //Fill inspection times
+            inspectionDropdown.DataSource = inspections;
+            inspectionTime = 0;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e){
+            if (e.KeyCode == Keys.Space){
+                if (!enabled){
+                    label1.ForeColor = Color.LightGreen;
+                }else{
+                    label1.ForeColor = Color.Black;
+                    stopWatch();
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e){
+            if(e.KeyCode == Keys.Space){
+                if (!watch.IsRunning && !enabled) {
+                    enabled = true;
+                    if (inspectionDropdown.SelectedIndex == 0) {
+                        startWatch();
+                        label1.ForeColor = Color.Red;
+                    }else{
+                        startInspection(inspections[inspectionDropdown.SelectedIndex] * 1000);
+                        label1.ForeColor = Color.Blue;
+                    }
+                    e.Handled = true;
+                }else if (enabled){
+                    enabled = false;
+                }
+            }else if(e.KeyCode == Keys.Delete || e.KeyCode == Keys.Back){
+                deleteSelectedTime();
+            }
+        }
+        private void removeFromHistory_Click(object sender, EventArgs e){
+            deleteSelectedTime();
+        }
+        //Functions
+        public void startWatch(){
+            watch.Restart();
+            timer.Start();
+        }
+        public void stopWatch(){
+            watch.Stop();
+            timer.Stop();
+            //Record results as long as the timer was not stopped during the inspection time period
+            if (!iTimer.Enabled) {
+                //Add time and refresh statistics
+                history.Add(double.Parse(label1.Text));
+                refreshStats();
+            }
+            //Generate scramble
+            scrambleLabel.Text = "Scramble: " + generateScramble();
+        }
+        public async void startInspection(int time){
+            inspectionTime = time;
+            watch.Restart();
+            iTimer.Start();
+            await stopInspection();
+        }
+        public async Task stopInspection() {
+            await Task.Delay(inspectionTime);
+            label1.ForeColor = Color.Red;
+            startWatch();
+            iTimer.Stop();
+        }
+        public void updateTime(object sender, EventArgs e){
+            label1.Text = "" + String.Format("{0:0.00}", watch.ElapsedMilliseconds / 1000.0);
+            //Application.DoEvents();
+        }
+        public void updateInspection(object sender, EventArgs e) {
+            label1.Text = "" + String.Format("{0:0.00}", (((inspectionTime) - watch.ElapsedMilliseconds) / 1000.0));
+        }
+        public void refreshStats(){
+            if (history.Count == 0){
+                bestLabel.Text = "Best: n/a";
+                averageLabel.Text = "Average: n/a";
+            }else{
+                bestLabel.Text = "Best: " + history.Min();
+                averageLabel.Text = "Average: " + String.Format("{0:0.00}", history.Average());
+            }
+        }
+        public String generateScramble(){
+            Random random = new Random();
+            //25 random turns that do not cancel the previous one
+            List<String> scramble = new List<String>();
+            for (int i = 0; i < 25; i++){
+                String move = turns.ElementAt(random.Next(0, turns.Count())) + modifiers.ElementAt(random.Next(0, modifiers.Count()));
+                //Checks if there are at least 2 moves that have been alraedy generated. Makes sure two of the same move types will not coincide (Ex. F F or R2 R). Makes sure two moves do not cancel each other out (Ex. U D U' or L2 R L2)
+                if (scramble.Count <= 1 || (scramble[scramble.Count - 1].First() != move.First() && !cancelOut(scramble[scramble.Count - 2], move))){
+                    if(scramble.Count == 1 && scramble[scramble.Count - 1].First() == move.First()){
+                        i--;
+                        continue;
+                    }
+
+                    scramble.Add(move);
+                }else{
+                    i--;
+                }
+            }
+            return String.Join("  ", scramble);
+        }
+        public bool cancelOut(String moveA, String moveB){
+            //Does not cancel out a move made 2 turns ago. EX Moveset that would fall through the cracks) L F L'
+            if (moveA.First() != moveB.First() || (moveA.Length == 0 && moveB.Length == 0))
+                return false; //Different moves (L and R)
+            if (moveA.Length == 1 && moveB.Length == 2 && moveB.Last() == '\'')
+                return true; //F counters F'
+            if (moveB.Length == 1 && moveA.Length == 1 && moveA.Last() == '\'')
+                return true; //F' counters F
+            if (moveA.Length == 2 && moveB.Length == 2 && moveA.Last() == '2' && moveB.Last() == '2')
+                return true; //F2 counters F2
+            return false;
+        }
+        public void deleteSelectedTime(){
+            try{
+                int index = historyList.SelectedIndex;
+                history.RemoveAt(index);
+            }catch{
+                //
+            }
+            historyList.DataSource = null;
+            historyList.DataSource = history;
+            refreshStats();
+        }
+    }
+}
